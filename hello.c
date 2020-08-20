87a3e8bd70dbe2b9a9f1dd3baffb69106e3a2127
Usama Ismail <uismail@huskers.unl.edu>
2020-08-20 21:24:34 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Usama Ismail
+ * Date:   2020/18/08
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdlib.h>
+
+ int main(int argc, char **argv) {
+
+     printf("hello World!\n");
+
+     return 0;
+ }
+
+
